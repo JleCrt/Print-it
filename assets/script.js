b87e3e490c142aaccae25eb23e3a6b11e5e932a0
JleCrt <julie.crt0@gmail.com>
2024-03-12 15:19:49 +0100

    Arrows + dots

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,28 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrow_left = document.querySelector(".arrow_left");
+const arrow_right = document.querySelector(".arrow_right");
+const dots_div = document.querySelector(".dots");
+const banner = document.querySelector(".banner-img");
+const text = document.querySelector(".slide-text");
+
+
+arrow_left.addEventListener("click", slideBack);
+arrow_right.addEventListener("click", slideNext);
+function slideBack(){
+	console.log("ok gauche");
+};
+function slideNext() {
+	console.log("ok droite");
+};
+
+for (let i = 0; i < slides.length; i++) {
+	const dot = document.createElement("div");
+	dot.className = "dot";
+	dots_div.appendChild(dot);
+};
+const dots = document.querySelectorAll(".dot");
+dots[0].classList.add("dot_selected");
+let count = 0;
